f282f68052357ae6ba956fd7c93e8766678f0e7d
OOlleennaa <123268201+OOlleennaa@users.noreply.github.com>
2023-07-13 00:44:51 +0100

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,7 +1,5 @@
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import Notiflix from 'notiflix';
 
-document.body.style.backgroundColor = '#f7eff4';
-const form = document.querySelector('form.form');
 const options = {
   position: 'center-bottom',
   distance: '15px',
@@ -11,11 +9,39 @@ const options = {
   cssAnimationStyle: 'from-right',
 };
 
-form.addEventListener('click', onPromiseCreate);
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(event) {
+  event.preventDefault();
+  const { delay, step, amount } = event.currentTarget.elements;
+
+      for (let i = 0; i < amount.value; i++) {
+      let position = i + 1;
+      const delays = Number(delay.value) + step.value * i;
+
+      createPromise(position, delays)
+        .then(({ position, delay }) => {
+          Notiflix.Notify.success(
+            `✅ Fulfilled promise ${position} in ${delay}ms`
+          );
+        })
+        .catch(({ position, delay }) => {
+          Notiflix.Notify.failure(
+            `❌ Rejected promise ${position} in ${delay}ms`
+          );
+        });
+    }
+  
+
+  event.currentTarget.reset();
+}
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
+
     setTimeout(() => {
       if (shouldResolve) {
         resolve({ position, delay });
@@ -24,34 +50,4 @@ function createPromise(position, delay) {
       }
     }, delay);
   });
-}
-
-function onPromiseCreate(e) {
-  e.preventDefault();
-  const { delay, step, amount } = e.currentTarget.elements;
-  let inputDelay = Number(delay.value);
-  let inputStep = Number(step.value);
-  let inputAmount = Number(amount.value);
-
-  // for (let i = 1; i <= inputAmount; i += 1) {
-  //   inputDelay += inputStep;
-
-  for (let i = 0; i < inputAmount; i += 1) {
-    inputDelay += inputStep * i; 
-
-    createPromise(i, inputDelay)
-      .then(({ position, delay }) => {
-        Notify.success(
-          `✅ Fulfilled promise ${position} in ${delay}ms`,
-          options
-        );
-      })
-      .catch(({ position, delay }) => {
-        Notify.failure(
-          `❌ Rejected promise ${position} in ${delay}ms`,
-          options
-        );
-      });
-    e.currentTarget.reset();
-  }
 }
